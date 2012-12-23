5c8d361ea9376c23f5ee0f90644166e4661ead15
remkvadriga <remkvadriga@192.168.0.102>
2012-12-23 15:30:07 +0400

    show done

--- src/Tour.java
+++ src/Tour.java
@@ -9,6 +9,8 @@
  */
 public class Tour {
 
+	private Node first;
+
 	private class Node {
 		private Point p;
 		private Node next;
@@ -21,6 +23,9 @@ public class Tour {
 	}
 
 	public Tour() {
+		Point p = new Point(1, 1);
+		first = new Node(p);
+
 	}
 
 	public Tour(Point a, Point b, Point c, Point d) {
@@ -28,10 +33,28 @@ public class Tour {
 	}
 
 	void show() {
+		Node temp = first;
+		int i = 1;
+		while (!(temp == first) || i==1) {
+			//TODO  Вынести наружу
+			StdOut.println("Point " + i + temp.p);
+			i++;
+			temp=temp.next;
+
+		}
 
 	}
 
 	void draw() {
+		Node temp = first;
+		int i = 1;
+		while (!(temp == first) || i==1) {
+			temp.p.draw();
+			
+			i++;
+			temp=temp.next;
+
+		}
 
 	}
 
