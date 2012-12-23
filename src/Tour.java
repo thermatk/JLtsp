0d34574229ada30ab96acfa76750a699ba998d18
thermatk <thermatk@thermatk.com>
2012-12-23 13:26:26 +0400

    Created Tour class

---
+++ src/Tour.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author BigBOSS
+ *
+ */
+public class Tour {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
