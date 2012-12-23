0cc28ae9dcbd4a894be9bf5839a297d94c6e70d8
thermatk <thermatk@thermatk.com>
2012-12-23 14:41:34 +0400

    FORMAT!

--- src/Tour.java
+++ src/Tour.java
@@ -5,44 +5,48 @@
 /**
  * @author thermatk
  * @author remkvadriga
- *
+ * 
  */
 public class Tour {
-	
+
 	private class Node {
 		private Point p;
 		private Node next;
+
 		public Node(Point p) {
 			this.p = p;
 			this.next = null;
-			
+
 		}
 	}
-	
-	
-	public Tour()
-	{
-	}
-    public Tour (Point a, Point b, Point c, Point d) {
-    	
-    }
-void show(){
-	
-}
-void draw(){
-	
-}
-double distance(){
-	
-}
-int size(){
-	
-}
-void insertSmallest(Point p){
-	
-}
 
-	
+	public Tour() {
+	}
+
+	public Tour(Point a, Point b, Point c, Point d) {
+
+	}
+
+	void show() {
+
+	}
+
+	void draw() {
+
+	}
+
+	double distance() {
+
+	}
+
+	int size() {
+
+	}
+
+	void insertSmallest(Point p) {
+
+	}
+
 	/**
 	 * @param args
 	 */
@@ -51,4 +55,3 @@ void insertSmallest(Point p){
 
 	}
 }
-
